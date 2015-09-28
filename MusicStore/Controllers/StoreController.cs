be50bf6e8b75c4596fc03f74f616f1edaa5a150e
unknown <won@dev01003-nb.kg.octopusnetwork.org>
2015-09-28 22:38:26 +0600

    First commit

---
+++ MusicStore/Controllers/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MusicStore.Models;
+
+namespace MusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+
+        public ActionResult Index()
+        {
+            //return "Hello from Store.Index()";
+            var genres = new List<Genre>
+            {
+                new Genre {Name = "Blues"},
+                new Genre {Name = "Jazz"},
+                new Genre {Name = "Rock"}
+            };
+
+            return View(genres);
+        }
+
+        // GET: /Store/Browse?genre=Rock
+
+        public ActionResult Browse(string genre)
+        {
+            //return "Hello from Store.Browse()";
+            //return HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+            var genreModel = new Genre {Name = genre};
+            return View(genreModel);
+        }
+
+        // GET: /Store/Details/5
+
+        public ActionResult Details(int id)
+        {
+            //return "Hello from Store.Details()";
+            //return "Store.Details, Id = " + id;
+            var album = new Album {Title = "Album " + 5};
+            return View(album);
+        }
+    }
+}
